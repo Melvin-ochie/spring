e5a7ba8ceb776a041da52c96ac5f86a1941ac5aa
Melvin-ochie <98253679+Melvin-ochie@users.noreply.github.com>
2023-11-18 13:47:21 +0100

    Add files via upload

---
+++ flightBooking service /flightBooking-service/src/main/java/dev/melvin/i/ochie/flightBooking/service/FlightBookingServiceApplication.java
@@ -0,0 +1,14 @@
+package dev.melvin.i.ochie.flightBooking.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class FlightBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingServiceApplication.class, args);
+	}
+
+}
